533bf0ce1596e2f40d8d9905aad84a72fe22d304
Guest User <Guest@SmartWaves-MacBook-Pro-2.local>
2018-10-06 22:46:26 +0800

    Add appProduction, appDev, and appTest productFlavors on build.gradle

--- app/build/generated/source/greendao/com/weather/isaiahj/androidweather/data/db/model/DaoMaster.java
+++ app/build/generated/source/greendao/com/weather/isaiahj/androidweather/data/db/model/DaoMaster.java
@@ -21,16 +21,16 @@ public class DaoMaster extends AbstractDaoMaster {
 
     /** Creates underlying database table using DAOs. */
     public static void createAllTables(Database db, boolean ifNotExists) {
+        OptionDao.createTable(db, ifNotExists);
         UserDao.createTable(db, ifNotExists);
         QuestionDao.createTable(db, ifNotExists);
-        OptionDao.createTable(db, ifNotExists);
     }
 
     /** Drops underlying database table using DAOs. */
     public static void dropAllTables(Database db, boolean ifExists) {
+        OptionDao.dropTable(db, ifExists);
         UserDao.dropTable(db, ifExists);
         QuestionDao.dropTable(db, ifExists);
-        OptionDao.dropTable(db, ifExists);
     }
 
     /**
@@ -49,9 +49,9 @@ public class DaoMaster extends AbstractDaoMaster {
 
     public DaoMaster(Database db) {
         super(db, SCHEMA_VERSION);
+        registerDaoClass(OptionDao.class);
         registerDaoClass(UserDao.class);
         registerDaoClass(QuestionDao.class);
-        registerDaoClass(OptionDao.class);
     }
 
     public DaoSession newSession() {
